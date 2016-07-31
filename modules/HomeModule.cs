154c6fb26a2cf777af639b24d0934bc91a966989
russvetsper <russvetsper@gmail.com>
2016-07-31 14:35:18 -0700

    add rout to index and result cshtml

---
+++ modules/HomeModule.cs
@@ -0,0 +1,20 @@
+using Nancy;
+using System.Collections.Generic;
+
+namespace WordCountering
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+     {
+      Get["/"] = _ => {
+        return View["index.cshtml"];
+      };
+
+      Post["/result"] = _ => {
+        WordCounter  newWord = new WordCounter  ( Request.Form["new-word"], Request.Form["new-sentence"]);
+        return View["results.cshtml", newWord];
+      };
+    }
+  }
+}
